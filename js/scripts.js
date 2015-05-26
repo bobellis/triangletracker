447005c8de988b1b5dc2725275109b466d96838f
Ben Cornelis & Bob Ellis <student@epicodus.com>
2015-05-26 11:17:39 -0700

    add frontend

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,15 @@ var triangleType = function(side1, side2, side3) {
     return 'scalene';
   }
 };
+
+$(document).ready(function() {
+  $("form#sides").submit(function(event){
+    var side1 = parseInt($("#side1").val());
+    var side2 = parseInt($("#side2").val());
+    var side3 = parseInt($("#side3").val());
+
+    $("#triangle_type").text(triangleType(side1, side2, side3));
+    event.preventDefault();
+  });
+
+});
