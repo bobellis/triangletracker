5d56ed6c7836ccc558b24decd45270eccc2632a9
Ben Cornelis & Bob Ellis <student@epicodus.com>
2015-05-26 10:54:47 -0700

    add return two more specs for scalene and triangletest with code to pass specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 var triangleType = function(side1, side2, side3) {
-  if ((side1 === side2) && (side2 === side3)){
+  if ((side1 >= (side2 + side3)) || (side2 >= (side1 + side3)) || (side3 >= (side1 + side2))){
+    return 'not a triangle';
+  }
+  else if ((side1 === side2) && (side2 === side3)){
     return 'equilateral';
   } else if ((side1 === side2) || (side2 === side3) || (side1 === side3)){
     return 'isosceles';
+  } else {
+    return 'scalene';
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,12 @@ describe('triangleType', function() {
   it('returns isosceles if two sides are equal', function(){
     expect(triangleType(2, 3, 2)).to.equal('isosceles');
   });
+
+  it('returns scalene if no sides are equal', function(){
+    expect(triangleType(3,4,6)).to.equal('scalene');
+  });
+
+  it('returns not a triangle if one side is the same or longer than the other two combined', function() {
+    expect(triangleType(2,2,8)).to.equal('not a triangle');
+  });
 });
